5ebf297528ce9213804a3748e44b3584ae69fe16
Aleksandr Pasharin <aleksandr.pasharin@columbiaroad.com>
2024-07-15 15:47:39 +0300

    quick fix for ts error on release details page

--- components/Releases/ReleaseDetails.tsx
+++ components/Releases/ReleaseDetails.tsx
@@ -3,6 +3,7 @@ import type { Release, TableRowInfo } from "./types";
 
 import {
   MatrixRunout,
+  StringOrStringArray,
   ValidCatNumbersObject,
   ValidatedCatNumbers,
   ValidatedCountriesType,
@@ -107,7 +108,9 @@ const processCatNumberObject = (
       : "cat_numbers" in catNumbersObject
       ? Array.isArray(catNumbersObject.cat_numbers)
         ? catNumbersObject.cat_numbers.join(", ")
-        : `in UK: ${catNumbersObject.cat_numbers["in UK"]}, in Europe: ${catNumbersObject.cat_numbers["in Europe"]}`
+        : "in UK" in catNumbersObject.cat_numbers
+        ? `in UK: ${catNumbersObject.cat_numbers["in UK"]}, in Europe: ${catNumbersObject.cat_numbers["in Europe"]}`
+        : "" // TODO: missing case of CD/slipcase properties
       : "(no catalogue number)";
 
   return labels.map((l) => [l, catNumbers]);
@@ -127,25 +130,28 @@ const processCatNumbers = (
   return processCatNumberObject(catNumbers);
 };
 
-const processMatrixRunout = (
-  matrixRunout: MatrixRunout | undefined
-): TableRowInfo[] => {
-  if (!matrixRunout) {
-    return [];
-  }
+// const processMatrixRunout = (
+//   matrixRunout: MatrixRunout | undefined
+// ): TableRowInfo[] => {
+//   if (!matrixRunout) {
+//     return [];
+//   }
 
-  if (typeof matrixRunout === "string") {
-    return [{ label: "Matrix / runout", value: matrixRunout }];
-  }
+//   if (typeof matrixRunout === "string") {
+//     return [{ label: "Matrix / runout", value: matrixRunout }];
+//   }
 
-  return Object.entries(matrixRunout).map(([key, value]) => ({
-    label: `Matrix / runout (${key})`,
-    value,
-  }));
-};
+//   return Object.entries(matrixRunout).map(([key, value]) => ({
+//     label: `Matrix / runout (${key})`,
+//     value,
+//   }));
+// };
 
 const countriesToString = (
-  countries: ValidatedCountriesType | undefined
+  countries:
+    | ValidatedCountriesType
+    | { "printed in": StringOrStringArray }
+    | undefined
 ): string | null => {
   if (!countries) {
     return null;
@@ -159,10 +165,23 @@ const countriesToString = (
     return countries.join(", ");
   }
 
-  const madeIn = countries["made in"];
-  const printedIn = countries["printed in"];
+  if ("made in" in countries) {
+    const madeIn = countries["made in"];
+    const printedIn = countries["printed in"];
 
-  return `made in: ${countriesToString(
-    madeIn
-  )}, printed in: ${countriesToString(printedIn)}`;
+    return `made in: ${countriesToString(
+      madeIn
+    )}, printed in: ${countriesToString(printedIn)}`;
+  }
+
+  if ("printed in" in countries) {
+    return `printed in: ${countriesToString(countries["printed in"])}`;
+  }
+
+  const cd = countries["CD"];
+  const slipcase = countries["slipcase"];
+
+  return `CD: ${countriesToString(cd)}, slipcase: ${countriesToString(
+    slipcase
+  )}`;
 };
