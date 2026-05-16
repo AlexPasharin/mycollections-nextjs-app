2614c8f7ef0bdd705432628751a628592c25fba6
Aleksandr Pasharin <aleksandr.pasharin@columbiaroad.com>
2026-05-16 19:19:32 +0300

    implement new artists view

--- db/prisma/index.ts
+++ db/prisma/index.ts
@@ -1,4 +1,26 @@
+import type { artists as Artist, musical_entries as MusicalEntry } from "generated/prisma/client";
 import { prisma } from "./client";
 
-export const getArtists = async () => prisma.artists.findMany();
+export const getArtists = async (): Promise<Artist[]> => prisma.artists.findMany();
 
+export const getArtistByName = async (artistName: string): Promise<Artist | null> => prisma.artists.findFirst({
+  where: {
+    name: artistName,
+  },
+});
+
+export const getArtistEntries = async (artistId: string) => prisma.musical_entries.findMany({
+  include: {
+    types_of_musical_entries: { include: { musical_entry_types: true } },
+  },
+  where: {
+    musical_entries_artists: {
+      some: {
+        artist_id: artistId,
+      },
+    },
+  },
+  orderBy: {
+    original_release_date: "asc",
+  },
+})
--- layouts/MainDirectory.tsx
+++ layouts/MainDirectory.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 export default function MainDirectory({ header }: { header: string }) {
   const sections = [
     { title: "Music Collection", path: "/music" },
+    { title: "Music Collection - New implementation (under construction)", path: "/music-collection" },
     { title: "Movies", path: "/movies" },
     { title: "Queen Discography", path: "/Queen/discography" },
   ];
